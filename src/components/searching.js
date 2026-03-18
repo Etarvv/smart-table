f0acee8f1932cd8eeee2da009b4ecc5eeaea957d
Leonid Tkachenko <etarvv@yandex.ru>
2026-03-19 00:08:54 +0300

    final?

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,8 +3,8 @@ import { rules, createComparison } from "../lib/compare.js";
 export function initSearching(searchField) {
       // @todo: #5.1 — настроить компаратор
       const compare = createComparison(
-            ["skipEmptyTargetValues"],
             [rules.searchMultipleFields(searchField, ["date", "customer", "seller"], false)]
+            ["skipEmptyTargetValues"],
       );
       return (data, state, action) => {
             // @todo: #5.2 — применить компаратор
--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,35 +1,29 @@
 import { sortCollection, sortMap } from "../lib/sort.js";
 
 export function initSorting(columns) {
-      return (data, state, action) => {
-            let field = null;
-            let order = null;
+    return (data, state, action) => {
+        if (action && action.name === "sort") {
+            const currentSortState = action.dataset.value;
+            const nextSortState = sortMap[currentSortState];
+            action.dataset.value = nextSortState;
 
-            if (action && action.name === "sort") {
-                  // @todo: #3.1 — запомнить выбранный режим сортировки
-                  const currentSortState = action.dataset.value;
-                  const nextSortState = sortMap[currentSortState];
-                  action.dataset.value = nextSortState;
-                  field = action.dataset.field;
-                  order = nextSortState;
+            columns.forEach((column) => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = "none";
+                }
+            });
+        }
 
-                  // @todo: #3.2 — сбросить сортировки остальных колонок
-                  columns.forEach((column) => {
-                        // Перебираем элементы (в columns у нас массив кнопок)
-                        if (column.dataset.field !== action.dataset.field) {
-                              // Если это не та кнопка, что нажал пользователь
-                              column.dataset.value = "none"; // тогда сбрасываем её в начальное состояние
-                        }
-                  });
-                  // @todo: #3.3 — получить выбранный режим сортировки
-                  columns.forEach((column) => {
-                        if (column.dataset.value !== "none") {
-                              field = column.dataset.field;
-                              order = column.dataset.value;
-                        }
-                  });
+        let field = null;
+        let order = null;
+        for (const column of columns) {
+            if (column.dataset.value !== "none") {
+                field = column.dataset.field;
+                order = column.dataset.value;
+                break;
             }
+        }
 
-            return sortCollection(data, field, order);
-      };
+        return sortCollection(data, field, order);
+    };
 }
