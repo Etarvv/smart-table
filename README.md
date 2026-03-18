714a1258d58038804bfa3db091c5409571656dac
Leonid Tkachenko <etarvv@yandex.ru>
2026-03-18 22:28:07 +0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+https://github.com/Etarvv/smart-table
 # sp6-2_smart-table_starter
--- src/components/sorting.js
+++ src/components/sorting.js
@@ -7,10 +7,22 @@ export function initSorting(columns) {
 
         if (action && action.name === 'sort') {
             // @todo: #3.1 — запомнить выбранный режим сортировки
+            const currentSortState = action.dataset.value;
+            const nextSortState = sortMap[currentSortState];
 
             // @todo: #3.2 — сбросить сортировки остальных колонок
-        } else {
+        columns.forEach(column => {                                    // Перебираем элементы (в columns у нас массив кнопок)
+        if (column.dataset.field !== action.dataset.field) {    // Если это не та кнопка, что нажал пользователь
+        column.dataset.value = 'none';                        // тогда сбрасываем её в начальное состояние
+        }
+        });
             // @todo: #3.3 — получить выбранный режим сортировки
+         columns.forEach((column) => {
+        if (column.dataset.value !== "none") {
+          field = column.dataset.field;
+          order = column.dataset.value;
+        }
+        });
         }
 
         return sortCollection(data, field, order);
